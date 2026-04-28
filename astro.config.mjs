a689a7b3a4490f652fa02dc8b6f26d0ed3a1af17
Shamveel P <shamveelp007@gmail.com>
2026-04-28 12:58:06 +0530

    feat - htaccess updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.packershub.in',
+  trailingSlash: 'always',
+  redirects: {
+    '/nellore-packers-movers': '/packers-movers-nellore',
+    '/hyderabad-packers-movers': '/packers-movers-hyderabad',
+    '/chennai-packers-movers': '/packers-movers-chennai',
+    '/bangalore-packers-movers': '/packers-movers-bengaluru',
+    '/vijayawada-packers-movers': '/packers-movers-vijayawada',
+  }
+});
